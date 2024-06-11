5685d61c76ccdb7ae1e4b22f5267135721f8edeb
Prince <principe3219@icloud.com>
2024-06-10 21:16:41 -0700

    started proj

---
+++ app.js
@@ -0,0 +1,74 @@
+const cardArray = [
+    {
+        name: 'blm',
+        img: 'img/blm.png',
+    },
+    {
+        name: 'brd',
+        img: 'img/brd.png',
+    },
+    {
+        name: 'drg',
+        img: 'img/drg.png',
+    },
+    {
+        name: 'mnk',
+        img: 'img/brd.mnk',
+    },
+    {
+        name: 'nin',
+        img: 'img/nin.png',
+    },
+    {
+        name: 'smn',
+        img: 'img/smn.png',
+    },
+    {
+        name: 'blm',
+        img: 'img/blm.png',
+    },
+    {
+        name: 'brd',
+        img: 'img/brd.png',
+    },
+    {
+        name: 'drg',
+        img: 'img/drg.png',
+    },
+    {
+        name: 'mnk',
+        img: 'img/brd.mnk',
+    },
+    {
+        name: 'nin',
+        img: 'img/nin.png',
+    },
+    {
+        name: 'smn',
+        img: 'img/smn.png',
+    }
+]
+
+cardArray.sort(() => 0.5 - Math.random())
+
+const gridDisplay = document.querySelector('#grid')
+
+function createBoard() {
+for (let i = 0; i < cardArray.length; i++) {
+   const card = document.createElement('img')
+   card.setAttribute('src', 'img/dpsrole.png')
+   card.setAttribute('data-id', i)
+   card.addEventListener('click', flipCard)
+   gridDisplay.appendChild(card)
+
+}
+}
+
+createBoard()
+
+function flipCard() {
+    console.log(cardArray)
+const cardId = this.getAttribute('data-id')
+console.log(cardArray[cardId].name)
+console.log('clicked', cardId)
+}
